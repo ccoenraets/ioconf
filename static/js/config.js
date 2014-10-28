b8f972154662a45f9a5ba1f6f8e1703f289f28a1
ccoenraets <ccoenraets@gmail.com>
2014-10-28 00:21:41 -0400

    fixes

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,5 @@
 angular.module('conference.config', [])
 
-    .constant('FB_APP_ID', '1687934808098911')
+    .constant('FB_APP_ID', '932536800093017')
 
-    .constant('SERVER_PATH','http://ioconf.herokuapp.com')
-
-    .constant('PUSH_SERVER_URL','http://pushadmin.herokuapp.com');
+    .constant('SERVER_PATH','http://ioconf.herokuapp.com');
